b2b54742743eaee68227846239519f1354077741
safwanshamsir99 <safwanshamsir7177@gmail.com>
2024-03-04 16:53:29 +0800

    saja commit

--- main.py
+++ main.py
@@ -1,40 +1,37 @@
+from typing import Union
+
 from fastapi import FastAPI
 from pydantic import BaseModel
 
 app = FastAPI()
 
 
-class Item(BaseModel):
-    name: str
-    description: str | None = None
-    price: float
-    tax: float = 10.5
+class BaseItem(BaseModel):
+    description: str
+    type: str
+
+
+class CarItem(BaseItem):
+    type: str = "car"
+
+
+class PlaneItem(BaseItem):
+    type: str = "plane"
+    size: int
 
 
 items = {
-    "foo": {"name": "Foo", "price": 50.2},
-    "bar": {"name": "Bar", "description": "The Bar fighters", "price": 62, "tax": 20.2},
-    "baz": {
-        "name": "Baz",
-        "description": "There goes my baz",
-        "price": 50.2,
-        "tax": 10.5,
+    "item1": {"description": "All my friends drive a low rider", "type": "car"},
+    "item2": {
+        "description": "Music is my aeroplane, it's my aeroplane",
+        "type": "plane",
+        "size": 5,
     },
 }
 
 
-@app.get(
-    "/items/{item_id}/name",
-    response_model=Item,
-    response_model_include=["name", "description"],
-)
-async def read_item_name(item_id: str):
+@app.get("/items/{item_id}", response_model=Union[PlaneItem, CarItem])
+async def read_item(item_id: str):
     return items[item_id]
 
-
-@app.get("/items/{item_id}/public", response_model=Item, response_model_exclude=["tax"])
-async def read_item_public_data(item_id: str):
-    return items[item_id]
-
-
-# Extra Models 
+# Union of anyOf
