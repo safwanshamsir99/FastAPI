4ddfe1696e8438c4cb0e2e354f64782afde6d9b1
safwanshamsir99 <safwanshamsir7177@gmail.com>
2024-03-08 16:23:47 +0800

    saja commit

--- main.py
+++ main.py
@@ -1,20 +1,27 @@
-from datetime import datetime
-from fastapi import FastAPI
-from fastapi.encoders import jsonable_encoder
-from pydantic import BaseModel
+from typing import Annotated
 
-fake_db = {}
-
-class Item(BaseModel):
-    title: str
-    timestamp: datetime
-    description: str | None = None
+from fastapi import Depends, FastAPI
 
 app = FastAPI()
 
-@app.put("/items/{id}")
-def update_item(id: str, item: Item):
-    json_compatible_item_data = jsonable_encoder(item)
-    fake_db[id] = json_compatible_item_data
 
-# Body update 
+fake_items_db = [{"item_name": "Foo"}, {"item_name": "Bar"}, {"item_name": "Baz"}]
+
+
+class CommonQueryParams:
+    def __init__(self, q: str | None = None, skip: int = 0, limit: int = 100):
+        self.q = q
+        self.skip = skip
+        self.limit = limit
+
+
+@app.get("/items/")
+async def read_items(commons: Annotated[CommonQueryParams, Depends(CommonQueryParams)]):
+    response = {}
+    if commons.q:
+        response.update({"q": commons.q})
+    items = fake_items_db[commons.skip : commons.skip + commons.limit]
+    response.update({"items": items})
+    return response
+
+# Sub-dependencies
