073dc7bd5003afa33f6a859ba725f7922aaa1427
safwanshamsir99 <safwanshamsir7177@gmail.com>
2024-04-16 11:38:22 +0800

    update files

--- main.py
+++ main.py
@@ -1,15 +1,7 @@
-import uvicorn
 from fastapi import FastAPI
 
 app = FastAPI()
 
-
 @app.get("/")
-def root():
-    a = "a"
-    b = "b" + a
-    return {"hello world": b}
-
-
-if __name__ == "__main__":
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+def read_root():
+    return {"Hello": "World"}
